b11eece93d15bdb80ed6fec155aa9e83055d975f
MSI\zuevf <zuev.f.s@gmail.com>
2022-07-25 15:39:13 +0300

    Fix Merge Bugs

--- Assets/Scripts/Loader/Loader.cs
+++ Assets/Scripts/Loader/Loader.cs
@@ -9,15 +9,7 @@ public class Loader : MonoBehaviour
 
     public void RestartScene()
     {
-<<<<<<< HEAD
-<<<<<<< HEAD
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-=======
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
->>>>>>> 6a899372e934c65c6e9a87b38c3ea6996688178e
-=======
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
->>>>>>> 6a899372e934c65c6e9a87b38c3ea6996688178e
     }
 
     public void StartNextLevel()
