b76aa78b1790dae112508bdc4871be6fe7fc02e3
rustamkhonoff <rustamkhonoff@gmail.com>
2022-07-25 16:50:14 +0500

    sdk fix

--- Assets/Scripts/Loader/Loader.cs
+++ Assets/Scripts/Loader/Loader.cs
@@ -9,7 +9,11 @@ public class Loader : MonoBehaviour
 
     public void RestartScene()
     {
+<<<<<<< HEAD
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+=======
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+>>>>>>> 6a899372e934c65c6e9a87b38c3ea6996688178e
     }
 
     public void StartNextLevel()
